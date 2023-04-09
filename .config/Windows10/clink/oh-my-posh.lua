3f4f6e790d770440e9f06a8c22974e2f15bc3c08
Kyle <kylemcintosh3@hotmail.co.uk>
2023-04-09 10:26:48 +0100

    Update oh-my-posh.lua

--- .config/Windows10/clink/oh-my-posh.lua
+++ .config/Windows10/clink/oh-my-posh.lua
@@ -1,1 +1,1 @@
-load(io.popen('oh-my-posh init cmd --config C:/dev/Github/dotfiles/.config/Windows10/oh-my-posh/.Shelltheme.omp.json'):read("*a"))()
+load(io.popen('oh-my-posh init cmd --config https://raw.githubusercontent.com/ClearlyKyle/dotfiles/main/.config/Windows10/oh-my-posh/.Shelltheme.omp.json'):read("*a"))()
